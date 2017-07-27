1b937deef6a604e898a239e61e363f5e89052940
Ryan Owens <RyanOwens@linux.com>
2017-07-27 11:56:47 -0500

    Camera class unit test, findcontours function

--- libarcbot/camera/camera.py
+++ libarcbot/camera/camera.py
@@ -1,37 +1,49 @@
-import numpy as np
-import cv2
 import os
+
+import cv2
+
 
 class Camera(object):
     """Camera Interface"""
-    def __init__(self, camera_id, testing = False):
+    __testing = False
+    __camera_id = 0
+    __capture = None
+
+    def __init__(self, camera_id, testing=False):
         super(Camera, self).__init__()
         self.__testing = testing
-        print(self.__testing)
         self.__camera_id = camera_id if camera_id > -1 else 0
         try:
-            self.openCamera()
-        except Exception as e:
+            self.opencamera()
+        except Exception:
             raise
 
-    def openCamera(self):
-        if self.__testing == False:
+    # noinspection PyArgumentList
+    def opencamera(self):
+        if not self.__testing:
             self.__capture = cv2.VideoCapture(self.__camera_id)
-            if self.__capture.isOpened() == False:
+            if not self.__capture.isOpened():
                 raise RuntimeError("Unable to open the camera")
         else:
             cwd = os.getcwd()
             image_url = cwd + "/testimages/testimage.png"
             try:
                 self.__capture = cv2.imread(image_url)
-            except Exception as e:
+            except Exception:
                 raise
 
-    def getFrame(self):
-        if self.__testing == True:
+    def getframe(self):
+        if self.__testing:
             return self.__capture
         flag, frame = self.__capture.read()
-        if flag == True:
+        if flag:
             return frame
         else:
             raise IOError("Unable to read from the Camera")
+
+    def findcountours(self, thresh_val, max_val, thresh_type=0):
+        frame = self.getframe()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray_frame, thresh_val, max_val, thresh_type)
+        image, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return image, contours, hierarchy
---
+++ tests/test_camera.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from libarcbot.camera.camera import Camera
+
+class TestCamera(TestCase):
+    def test_openCamera(self):
+        self.fail()
+
+    def test_getFrame(self):
+        self.fail()
+
+    def test_findCountours(self):
+        self.fail()
