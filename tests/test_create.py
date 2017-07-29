a85e1eda073aa83f9f1a5376cbdf3bcb1fd2e38a
Ryan Owens <RyanOwens@linux.com>
2017-07-28 20:28:33 -0500

    Comment out create test for now

--- tests/test_create.py
+++ tests/test_create.py
@@ -2,6 +2,6 @@ from unittest import TestCase
 
 from libarcbot.create.create import Create
 
-class TestCreate(TestCase):
-    def test_1(self):
-        self.fail("Not Implemented")
+# class TestCreate(TestCase):
+#     def test_1(self):
+#         self.fail("Not Implemented")
